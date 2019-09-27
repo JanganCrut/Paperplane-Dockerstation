9182e0a29348dc6e186d826d06cf1ab1b348db13
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-09-27 19:33:03 +0700

    skeleton : Initial spin for trimmed coredocker
    Co-authored-by: baalajimaestro <baalajimaestro@computer4u.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+exit_script()
+{
+echo "Build is dead, Jim!"
+sendTG "Docker Image Build for Paperplane is dead, Jim!"
+}
+
+trap exit_script SIGINT SIGTERM
+
+function sendTG() {
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001372533112&parse_mode=Markdown"
+}
+
+sendTG "\`Docker image is being updated!\`"
+
+docker build . -t raphielscape/paperplanebase:latest
+docker tag raphielscape/paperplanebase:latest raphielscape/paperplanebase:latest
+echo "$OWO" | docker login -u raphielscape --password-stdin >/dev/null 2>&1
+docker push raphielscape/paperplanebase
+
+sendTG "@raphielscape \`I have pushed new images to docker\` %0A [Images are Here](https://hub.docker.com/r/raphielscape/paperplanebase)"
